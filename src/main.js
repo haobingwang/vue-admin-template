32ef5bd78526a01ca8fd0ed5a38d7e97fb20aec4
wanghaobing <wanghb@fosun.com>
2018-11-30 10:03:57 +0800

    引入 log 库：loglevel

--- src/main.js
+++ src/main.js
@@ -11,6 +11,30 @@ import '@/styles/index.scss' // global css
 import moment from 'moment'
 Object.defineProperty(Vue.prototype, '$moment', { value: moment })
 
+// 引入 loglevel 记录日志
+import * as log from 'loglevel'
+
+// 在开发测试环境中记录所有消息
+console.log('log level: ' + log.getLevel())
+if (process.env.NODE_ENV === 'development') {
+  log.setLevel('trace')
+}
+log.info('log level: ' + log.getLevel())
+
+// log.trace('trace 消息')
+// log.debug('debug 消息')
+// log.info('info 消息')
+// log.warn('warn 消息')
+// log.error('error 消息')
+
+// 在 Vue 组件中使用
+Object.defineProperty(Vue.prototype, '$log', { value: log })
+// this.$log.trace('trace 消息')
+// this.$log.debug('debug 消息')
+// this.$log.info('info 消息')
+// this.$log.warn('warn 消息')
+// this.$log.error('error 消息')
+
 import App from './App'
 import router from './router'
 import store from './store'
